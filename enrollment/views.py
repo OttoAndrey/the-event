ef2a0922935ec8fcaba647b2b7abf4099071d3c4
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-20 01:14:00 +0700

    Add ParticipantSerializer

--- enrollment/views.py
+++ enrollment/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
 
@@ -11,12 +12,24 @@ class ApplicationSerializer(ModelSerializer):
         fields = ['contact_phone', 'ticket_type']
 
 
+class ParticipantSerializer(ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ['first_name', 'last_name', 'email']
+
+
 @api_view(['POST'])
 def enroll(request):
     serializer = ApplicationSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
-    participants = request.data.get('participants', [])  # TODO validate data!
+    participants = request.data.get('participants', [])
+    if not isinstance(participants, list):
+        raise ValidationError('Expects participants field be a list!')
+
+    for fields in participants:
+        serializer = ParticipantSerializer(data=fields)
+        serializer.is_valid(raise_exception=True)
 
     application = Application.objects.create(
         contact_phone=str(request.data['contact_phone']),
